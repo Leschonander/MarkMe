413a1fbcb52eaeccbaf86b96187561baadf0e739
Leschonander <33600597+Leschonander@users.noreply.github.com>
2018-04-17 23:12:29 -0400

    Updated Index File

--- index.js
+++ index.js
@@ -1,47 +1,78 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
-var personsLost = [
-    {firstName: "Mark" , lastName: "Dombroski" , lastSeen: "01/01/2018", notes: "In Memoriam"  },
-    {firstName: "Joachim" , lastName: "Smith" , lastSeen: "01/01/2018", notes: "Testing A" },
-    {firstName: "John" , lastName: "Smith" , lastSeen: "01/01/2018", notes: "Testing B" }
-];
+mongoose.connect('mongodb+srv://MYSELF:6lkp7DfBIKpoj57q@markmedatabase-4yie0.mongodb.net/test');
+var uri = "mongodb+srv://MYSELF:6lkp7DfBIKpoj57q@markmedatabase-4yie0.mongodb.net/test";
+MongoClient.connect(uri, function(err, client) {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
+var personSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    lastSeen: String,
+    notes: String
+});
+
+var Person = mongoose.model("Person", personSchema);
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
     res.render("Home");
 });
 
 app.get('/missing', function(req, res){
-    res.render("Missing", {personsLost: personsLost});
+    Person.find({}, function(err, allPersonsLost){
+        if(err){
+            console.log("Database Error");
+            console.log(err);
+        } else {
+            console.log("Sucess")
+            res.render("Missing", {personsLost: allPersonsLost});
+        }
+    });
 });
+
+app.get('/newmissing', function(req, res){
+    res.render('Newmissing')
+})
 
 app.post('/newmissing', function(req, res){
     var newFMissing = req.body.newmissingfname
     var NewLMissing = req.body.newmissinglname
-    var datelastsighted = JSON.stringify(req.body.datesighted)
+    var datelastsighted = req.body.datesighted
     var information = req.body.tips
-    personsLost.push({
-        firstName: req.body.newmissingfname , 
-        lastName: req.body.newmissinglname , 
-        lastSeen: req.body.datelastsighted ,
-        notes: req.body.tips
+    var newPersonLost = {
+      firstName: newFMissing , 
+      lastName: NewLMissing , 
+      lastSeen: datelastsighted , 
+      notes: information 
+    };
+    Person.create(newPersonLost, function(err,newPerson){
+        if(err){
+            console.log("Failure in creation.")
+            console.log(err);
+        } else {
+            res.redirect('/missing');
+            console.log(newPerson); 
+        }
     });
-    console.log(personsLost); 
-    res.redirect('/missing');
 });
-
 app.listen(3000, function(){
     console.log("Server is up!"); 
 });
 
-/*
-[ { firstName: 'Mark', lastName: 'Dombroski' },
-  { firstName: 'Joachim', lastName: 'Smith' },
-  { firstName: 'John', lastName: 'Smith' },
-  'WillMckay' ]
-*/
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Mongoose is up!")
+});
